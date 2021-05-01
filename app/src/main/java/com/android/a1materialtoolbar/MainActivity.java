a426517ea5bd51a516ce9510ee6d0bc0083459d7
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 09:21:37 +0700

    material toolbar

--- app/src/main/java/com/android/a1materialtoolbar/MainActivity.java
+++ app/src/main/java/com/android/a1materialtoolbar/MainActivity.java
@@ -1,14 +1,45 @@
 package com.android.a1materialtoolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Material Action Bar");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.settings){
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.search){
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
